0dcc7d8a82c3ff8dc6e84e5855ebde9ce10d1c08
JhessReyes <jhessaureyes@gmail.com>
2025-08-26 19:16:26 -0600

    not required

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,6 +7,6 @@ export const envs = {
   DB_PASSWORD: get("DB_PASSWORD").required().default("").asString(),
   DB_HOST: get("DB_HOST").required().default("").asString(),
   DB_PORT: get("DB_PORT").required().default("").asPortNumber(),
-  APP_PORT: get("APP_PORT").required().default(4000).asPortNumber(),
+  APP_PORT: get("APP_PORT").default(4000).asPortNumber(),
   NODE_ENV: get("NODE_ENV").default("development").asString(),
 };
