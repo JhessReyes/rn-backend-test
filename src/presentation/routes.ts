610ab15a968500181807269a07c2bf2b9e405d1d
JhessReyes <jhessaureyes@gmail.com>
2025-08-26 18:32:38 -0600

    add public

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import path from "path";
 
 export class AppRouter {
   public static get routes() {
     const router: Router = Router();
-
+    router.get("/", (_req: any, res: any) => {
+      const filePath = path.join(process.cwd(), "public", "page.html");
+      res.sendFile(filePath);
+    });
     // Get Teams
     router.get("/api/v1/teams", async (req, res) => {
       res.send("Teams");
@@ -27,7 +31,7 @@ export class AppRouter {
     // Add Favorite Superhero
     router.post("/api/v1/add-favorite", async (req, res) => {
       if (!req.body.id) res.json({ error: "No superhero id provided" });
-    
+
       res.send("Add Favorite");
     });
 
